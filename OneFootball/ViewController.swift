8011d0d36639c13574d63dcba3f5256d308b2fd8
Iskhak <izhtnv04@gmail.com>
2023-07-07 21:57:08 +0600

    initial commit

---
+++ OneFootball/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OneFootball
+//
+//  Created by Iskhak Zhutanov on 07.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+
+
+}
+
