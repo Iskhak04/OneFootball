29af409a715ee7a84b20ef3638e0445717b00638
Iskhak <izhtnv04@gmail.com>
2023-07-08 23:59:39 +0600

    onboarding view is done

--- OneFootball/Modules/Onboarding/OnboardingViewController.swift
+++ OneFootball/Modules/Onboarding/OnboardingViewController.swift
@@ -10,6 +10,8 @@ import SnapKit
 
 final class OnboardingViewController: UIViewController {
     
+    private let backgroundImages: [UIImage] = [UIImage(named: "messi")!, UIImage(named: "ronaldo")!, UIImage(named: "torres")!, UIImage(named: "spain")!]
+        
     private lazy var onboardingCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -20,12 +22,46 @@ final class OnboardingViewController: UIViewController {
         collectionView.delegate = self
         collectionView.register(OnboardingCollectionViewCell.self, forCellWithReuseIdentifier: "OnboardingCollectionViewCell")
         collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isScrollEnabled = false
+        collectionView.contentInsetAdjustmentBehavior = .never
         return collectionView
+    }()
+    
+    private lazy var indicatorDotsView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private lazy var firstIndicatorDotView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        view.layer.cornerRadius = 6
+        return view
+    }()
+    
+    private lazy var secondIndicatorDotView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 6
+        return view
+    }()
+    
+    private lazy var thirdIndicatorDotView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 6
+        return view
+    }()
+    
+    private lazy var fourthIndicatorDotView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 6
+        return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        onboardingCollectionView.contentInsetAdjustmentBehavior = .never
         view.backgroundColor = .systemBackground
         layout()
     }
@@ -35,6 +71,64 @@ final class OnboardingViewController: UIViewController {
         onboardingCollectionView.snp.makeConstraints { make in
             make.height.width.centerX.equalToSuperview()
         }
+        
+        view.addSubview(indicatorDotsView)
+        indicatorDotsView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-150)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(12)
+            make.width.equalTo(84)
+        }
+        
+        indicatorDotsView.addSubview(firstIndicatorDotView)
+        firstIndicatorDotView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview()
+            make.height.width.equalTo(12)
+        }
+        
+        indicatorDotsView.addSubview(secondIndicatorDotView)
+        secondIndicatorDotView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(firstIndicatorDotView.snp.right).offset(12)
+            make.height.width.equalTo(12)
+        }
+        
+        indicatorDotsView.addSubview(thirdIndicatorDotView)
+        thirdIndicatorDotView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(secondIndicatorDotView.snp.right).offset(12)
+            make.height.width.equalTo(12)
+        }
+        
+        indicatorDotsView.addSubview(fourthIndicatorDotView)
+        fourthIndicatorDotView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(thirdIndicatorDotView.snp.right).offset(12)
+            make.height.width.equalTo(12)
+        }
+    }
+    
+    @objc private func nextButtonClicked() {
+        let visibleCellIndexPathRow = onboardingCollectionView.indexPathsForVisibleItems.first!.row
+        
+        if visibleCellIndexPathRow != 3 {
+            onboardingCollectionView.scrollToItem(at: IndexPath(row: visibleCellIndexPathRow + 1, section: 0), at: [], animated: false)
+        }
+        
+        switch visibleCellIndexPathRow {
+        case 0:
+            secondIndicatorDotView.backgroundColor = .systemBackground
+            firstIndicatorDotView.backgroundColor = .gray
+        case 1:
+            thirdIndicatorDotView.backgroundColor = .systemBackground
+            secondIndicatorDotView.backgroundColor = .gray
+        case 2:
+            fourthIndicatorDotView.backgroundColor = .systemBackground
+            thirdIndicatorDotView.backgroundColor = .gray
+        default:
+            ()
+        }
     }
 }
 
@@ -43,12 +137,24 @@ extension OnboardingViewController: UICollectionViewDataSource, UICollectionView
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 4
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = onboardingCollectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCollectionViewCell", for: indexPath) as! OnboardingCollectionViewCell
-        cell.backgroundColor = .brown
+        
+        cell.nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        cell.backgroundImageView.image = backgroundImages[indexPath.row]
+        
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        if indexPath.row == 3 {
+            let lastCell = cell as! OnboardingCollectionViewCell
+            
+            lastCell.nextButton.setTitle("Start", for: .normal)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
--- OneFootball/Modules/Onboarding/Views/OnboardingCollectionViewCell.swift
+++ OneFootball/Modules/Onboarding/Views/OnboardingCollectionViewCell.swift
@@ -12,44 +12,66 @@ final class OnboardingCollectionViewCell: UICollectionViewCell {
     lazy var skipButton: UIButton = {
         let button = UIButton()
         button.setTitle("Skip", for: .normal)
+        button.titleLabel?.font = UIFont(name: Fonts.MontserratRegular.rawValue, size: 18)
         button.setTitleColor(.white, for: .normal)
         return button
     }()
     
+    lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.alpha = 1
+        return imageView
+    }()
+    
     lazy var bottomView: UIView = {
         let view = UIView()
-        view.backgroundColor = .systemPink
-        view.layer.cornerRadius = 20
+        view.backgroundColor = .label
+        view.layer.cornerRadius = 30
         return view
     }()
     
     lazy var mainFeatureLabel: UILabel = {
         let label = UILabel()
-        label.text = "Store your NFT collection"
-        label.textColor = .white
-        label.backgroundColor = .black
+        label.text = "Private sync across your devices"
+        label.textColor = .systemBackground
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.font = UIFont(name: Fonts.MontserratBold.rawValue, size: 35)
         return label
     }()
     
     lazy var mainFeatureDescriptionLabel: UILabel = {
         let label = UILabel()
         label.text = "This is a subscript that describes this feature in a better way"
-        label.textColor = .white
-        label.backgroundColor = .black
+        label.textColor = .systemBackground
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        label.font = UIFont(name: Fonts.MontserratRegular.rawValue, size: 18)
         return label
     }()
     
     lazy var nextButton: UIButton = {
         let button = UIButton()
         button.setTitle("Next", for: .normal)
-        button.backgroundColor = .white
-        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemBackground
+        button.setTitleColor(.label, for: .normal)
         button.layer.cornerRadius = 15
+        button.titleLabel?.font = UIFont(name: Fonts.MontserratSemiBold.rawValue, size: 18)
         return button
     }()
     
     override func layoutSubviews() {
         super.layoutSubviews()
+    
+        addSubview(backgroundImageView)
+        backgroundImageView.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview()
+            make.height.equalTo(frame.height / 2 + 125)
+        }
         
         addSubview(skipButton)
         skipButton.snp.makeConstraints { make in
@@ -60,19 +82,23 @@ final class OnboardingCollectionViewCell: UICollectionViewCell {
         addSubview(bottomView)
         bottomView.snp.makeConstraints { make in
             make.bottom.left.right.equalToSuperview()
-            make.height.equalTo(frame.height / 2 - 30)
+            make.height.equalTo(frame.height / 2 - 100)
         }
         
         bottomView.addSubview(mainFeatureLabel)
         mainFeatureLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(40)
-            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(10)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalTo(100)
         }
         
         bottomView.addSubview(mainFeatureDescriptionLabel)
         mainFeatureDescriptionLabel.snp.makeConstraints { make in
-            make.top.equalTo(mainFeatureLabel.snp.bottom).offset(30)
-            make.centerX.equalToSuperview()
+            make.top.equalTo(mainFeatureLabel.snp.bottom).offset(10)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalTo(60)
         }
         
         bottomView.addSubview(nextButton)
